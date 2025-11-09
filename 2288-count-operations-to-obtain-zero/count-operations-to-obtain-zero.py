5fbb346130a89d1aaaa95deddd597df017bb8205
Kaushik <boddula.kaushik@gmail.com>
2025-11-09 19:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (50.48%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        count=0
+        while min(num1,num2)>0:
+            if num1>=num2:
+                num1,num2=num2,num1
+            count+=num2//num1
+            num2%=num1
+        return count
