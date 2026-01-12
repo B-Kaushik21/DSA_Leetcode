17c24d7a3032fbb587271eacfc54d883262313d0
Kaushik <boddula.kaushik@gmail.com>
2026-01-12 23:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (5.81%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store={}
+        n=len(nums)
+        for i in range(n):
+            complement=target-nums[i]
+            if complement in store:
+                return [store[complement],i]
+            store[nums[i]]=i
+        return []
