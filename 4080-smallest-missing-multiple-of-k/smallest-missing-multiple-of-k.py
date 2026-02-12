6ef253dd8289c4d3f8ce566e1a25e596b167c918
Kaushik <boddula.kaushik@gmail.com>
2026-02-12 06:54:45 +0530

    Time: 4 ms (6.75%) | Memory: 19.4 MB (15.92%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -1,15 +1,12 @@
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
-        freq=set()
-        for i in range(len(nums)):
-            if nums[i]%k==0:
-                freq.add(nums[i])
-        if len(freq)==0:
-            return k
-        temp=max(freq)//k
-        length=len(freq)
-        print(length)
-        for i in range(1,length+1):
-            if k*i not in freq:
-                return k*i
-        return temp*k+k
+        sett=set()
+        for n in nums:
+            sett.add(n)
+        i=1
+        while True:
+            if i%k==0 and i not in sett:
+                return i
+            i+=1
+        
+        
