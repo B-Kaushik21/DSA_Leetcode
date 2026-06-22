68fc538dcd00fd467a9d5180a83959a4d126e5f6
Kaushik <boddula.kaushik@gmail.com>
2026-06-22 23:50:30 +0530

    Time: 3 ms (61.87%) | Memory: 19.3 MB (38.08%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        text=list(text)
-        res=0
-        while True:
-            word=list('balloon')
-            for ch in word:
-                found=False
-                for i in range(len(text)):
-                    if text[i]==ch:
-                        text[i]='*'
-                        found=True
-                        break
-                if not found:
-                    return res
-            res=res+1
+        b=a=l=o=n=0
+        for t in text:
+            if t=='b':
+                b+=1
+            elif t=='a':
+                a+=1
+            elif t=='l':
+                l+=1
+            elif t=='o':
+                o+=1
+            elif t=='n':
+                n+=1
+        return min(b,a,l//2,o//2,n)
